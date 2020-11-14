b2709222ca9236d0129c182b98daeaa104a0a772
Luk1Pr0 <lpytel16@gmail.com>
2020-11-14 17:38:37 +0100

    Added option to toggle between full screen and minimised player

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const player = document.querySelector(".player"); 
 const video = document.querySelector("video");
 const progressRange = document.querySelector(".progress-range");
 const progressBar = document.querySelector(".progress-bar");
@@ -105,7 +106,39 @@ function changeSpeed() {
 
 
 // Fullscreen ------------------------------- //
+function openFullscreen(elem) {
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) { /* Safari */
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE11 */
+        elem.msRequestFullscreen();
+    }
+    video.classList.add("video-fullscreen");
+}
+  
+  /* Close fullscreen */
+function closeFullscreen() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) { /* Safari */
+        document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { /* IE11 */
+        document.msExitFullscreen();
+    }
+    video.classList.remove("video-fullscreen");
+}
 
+let isFullscreen = false;
+
+function toggleFullscreen() {
+    if (!isFullscreen) {
+        openFullscreen(player);
+    } else {
+        closeFullscreen();
+    }
+    isFullscreen = !isFullscreen;
+}
 
 // Event listeners
 playBtn.addEventListener("click", togglePlay);
@@ -116,3 +149,4 @@ progressRange.addEventListener("click", setProgress);
 volumeRange.addEventListener("click", changeVolume);
 volumeIcon.addEventListener("click", toggleMute);
 speed.addEventListener("change", changeSpeed);
+fullscreenBtn.addEventListener("click", toggleFullscreen);
