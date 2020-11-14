079c2dcaf414457810207d6deac96a201d4b55c6
Luk1Pr0 <lpytel16@gmail.com>
2020-11-14 16:49:59 +0100

    Video mute icon is now toggling depending on if the video is muted or not

--- script.js
+++ script.js
@@ -55,6 +55,8 @@ function setProgress(e) {
 }
 
 // Volume Controls --------------------------- //
+let lastVolume = "1";
+
 function changeVolume(e) {
     let volume = e.offsetX / volumeRange.offsetWidth;
     // Roundup the volume value
@@ -75,6 +77,23 @@ function changeVolume(e) {
     } else if (volume === 0) {
         volumeIcon.classList.add("fas", "fa-volume-off");
     }
+    lastVolume = volume;
+}
+
+function toggleMute() {
+    volumeIcon.className = "";
+    if (video.volume) {
+        lastVolume = video.volume;
+        video.volume = 0;
+        volumeBar.style.width = 0;
+        volumeIcon.classList.add("fas", "fa-volume-mute");
+        volumeIcon.setAttribute("title", "Unmute");
+    } else {
+        video.volume = lastVolume;
+        volumeBar.style.width = `${lastVolume * 100}%`;
+        volumeIcon.classList.add("fas", "fa-volume-up");
+        volumeIcon.setAttribute("title", "Mute");
+    }
 }
 
 
@@ -92,3 +111,4 @@ video.addEventListener("timeupdate", updateProgress);
 video.addEventListener("canplay", updateProgress);
 progressRange.addEventListener("click", setProgress);
 volumeRange.addEventListener("click", changeVolume);
+volumeIcon.addEventListener("click", toggleMute);
