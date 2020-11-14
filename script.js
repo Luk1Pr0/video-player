0bbb82ff82201f77b35552735bdffeb7b5b3a92f
Luk1Pr0 <lpytel16@gmail.com>
2020-11-14 16:25:05 +0100

    Volume is now adjustable via the player volume feature

--- script.js
+++ script.js
@@ -55,7 +55,19 @@ function setProgress(e) {
 }
 
 // Volume Controls --------------------------- //
-
+function changeVolume(e) {
+    let volume = e.offsetX / volumeRange.offsetWidth;
+    // Roundup the volume value
+    if (volume < 0.1) {
+        volume = 0;
+    }
+    if (volume > 0.9) {
+        volume = 1;
+    }
+    volumeBar.style.width = `${volume * 100}%`;
+    video.volume = volume;
+    console.log(volume);
+}
 
 
 // Change Playback Speed -------------------- //
@@ -71,3 +83,4 @@ video.addEventListener("click", togglePlay);
 video.addEventListener("timeupdate", updateProgress);
 video.addEventListener("canplay", updateProgress);
 progressRange.addEventListener("click", setProgress);
+volumeRange.addEventListener("click", changeVolume);
